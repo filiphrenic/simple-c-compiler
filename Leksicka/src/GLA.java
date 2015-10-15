65336c6abde5348108d720b6f325b7f4461697f1
Filip Hrenic <hrenic.filip@gmail.com>
2015-10-15 17:49:05 +0200

    added javadoc to GLA and LA

--- Leksicka/src/GLA.java
+++ Leksicka/src/GLA.java
@@ -6,6 +6,8 @@ import hr.fer.zemris.ppj.stream.InputParser;
 import hr.fer.zemris.ppj.stream.Streamer;
 
 /**
+ * This class creates a generator, reads in definitions, creates automatons and
+ * writes them to the file that will be read by the lexical analyzer.
  * 
  * @author fhrenic
  */
@@ -17,6 +19,12 @@ public class GLA {
 
     private InputStream input;
 
+    /**
+     * Creates a new GLA (generator) whose job is to generate objects that will
+     * be used by the LA (lexical analyzer)
+     * 
+     * @param input input stream used to read in the definitions
+     */
     public GLA(InputStream input) {
         this.input = input;
     }
--- Leksicka/src/analizator/LA.java
+++ Leksicka/src/analizator/LA.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package analizator;
 
 import java.io.IOException;
@@ -15,22 +12,38 @@ import hr.fer.zemris.ppj.lex.LexRule;
 import hr.fer.zemris.ppj.stream.Streamer;
 
 /**
+ * This class reads the automatons from a file, creates a lexical analyzer based
+ * on those automatons and analyzes the input
+ * 
  * @author fhrenic
  */
 public class LA {
 
     public static void main(String[] args) {
         // TODO
+        // create input & output streams, create LA object and perform analysis
+
     }
 
     private InputStream input;
     private OutputStream output;
 
+    /**
+     * Creates a new Lexical Analyzer that analyzes the input stream and prints
+     * the results to the output stream
+     * 
+     * @param input input stream to analyze
+     * @param output output stream to show the results
+     */
     public LA(InputStream input, OutputStream output) {
         this.input = input;
         this.output = output;
     }
 
+    /**
+     * Performs lexical analysis of the input stream.
+     */
+    @SuppressWarnings("unchecked")
     public void lexicalAnalysis() {
         try (ObjectInputStream stream = Streamer.getInput()) {
             String startState = (String) stream.readObject();
