9dedba2c58be638c9e0b4fbc39a28c70cac15f82
dnakic <domagoj.nakic@fer.hr>
2015-11-11 02:15:59 +0100

    Dodano racunanje skupova ZAPOCINJE.

--- Leksicka/src/hr/fer/zemris/ppj/sintax/grammar/Grammar.java
+++ Leksicka/src/hr/fer/zemris/ppj/sintax/grammar/Grammar.java
@@ -2,10 +2,12 @@ package hr.fer.zemris.ppj.sintax.grammar;
 
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeSet;
+import java.util.function.Predicate;
 
 import hr.fer.zemris.ppj.automaton.DFAExtended;
 import hr.fer.zemris.ppj.automaton.EpsilonNFA;
@@ -27,6 +29,9 @@ public class Grammar {
     private List<Symbol> terminalSymbols;
     private Map<Symbol, List<Production>> productions;
     private Production startingProduction; // S' -> S
+    
+    private Map<Symbol, Set<Symbol>> firstSets;
+//    private Map<Symbol, Set<Symbol>> followSets;
 
     private DFAExtended<LREntry, Symbol> dfa;
 
@@ -41,6 +46,9 @@ public class Grammar {
 
         // finds empty symbols
         annotateEmptySymbols();
+        
+        constructFirstSets();
+//        followSets = new FollowRelation(this).getFollowSets();
 
         for (Symbol s : terminalSymbols) {
             System.out.println(s + " " + s.isEmpty());
@@ -49,6 +57,18 @@ public class Grammar {
         // zadnja metoda u konstruktoru
         //generateDFA();
     }
+    
+    public List<Symbol> getTerminalSymbols() {
+    	return terminalSymbols;
+    }
+    
+    public Set<Symbol> getNonTerminalSymbols() {
+    	return productions.keySet();
+    }
+    
+//    public Map<Symbol, List<Production>> getAllProductions() {
+//    	return productions;
+//    }
 
     private void annotateEmptySymbols() {
         boolean foundNewEmpty = false;
@@ -69,7 +89,7 @@ public class Grammar {
         }
     }
 
-    private Production getStartingProduction() {
+    public Production getStartingProduction() {
         return startingProduction;
     }
 
@@ -77,18 +97,19 @@ public class Grammar {
         // zapocinje znakom
         // zavrsni zavrsava jedino sam sa sobom
         // nezavrsni zavrsava s refleksivnim tranzitivnim okruzenjem...
-        return null;
+        return firstSets.get(sym);
     }
 
     private Set<Symbol> follows(Symbol sym) {
         // SLIJEDI(x) = skup svih zavrsnih
-        return null;
+//        return followSets.get(sym);
+    	return null;
     }
 
-    private List<Production> getProductionsForLhs(Symbol sym) {
+    public List<Production> getProductionsForLhs(Symbol sym) {
         // vraca sve produkcije za neki nezavrsni znak
         // tipa za A vraca produkcije A->a; A->bBaA; ...
-        return null;
+        return productions.get(sym);
     }
 
     private List<Production> getProductions() {
@@ -194,6 +215,74 @@ public class Grammar {
             }
         }
         return sb.toString();
+    }
+    
+    private void constructFirstSets() {
+        firstSets = new HashMap<Symbol, Set<Symbol>>();
+        // za svaki simbol nadji set simbola ZAPOCINJE(simbol)
+
+        // izravno zapocinje
+        for(Symbol symbol : terminalSymbols) {
+        	Set<Symbol> firstSet = new HashSet<Symbol>();
+        	firstSet.add(symbol);
+        	firstSets.put(symbol, firstSet);
+        }
+        
+        Set<Symbol> nonTerminalSymbols = productions.keySet();
+        for(Symbol symbol : nonTerminalSymbols) {
+        	Set<Symbol> firstSet = new HashSet<Symbol>();
+            
+        	for(Production production : productions.get(symbol)) {
+            	for(Symbol rhsSym : production.getRHS()) {
+            		firstSet.add(rhsSym);
+            		if(!rhsSym.isEmpty()) {
+            			break;
+            		}
+            	}	
+            }
+
+    		firstSets.put(symbol, firstSet);
+    	}
+
+    	// ref. tran. okruzenje
+        boolean change;
+        do {
+        	change = false;
+	        for(Symbol symbol : nonTerminalSymbols) {
+	    		Set<Symbol> firstSet = firstSets.get(symbol);
+	    		Set<Symbol> tmp = new HashSet<Symbol>(firstSet);
+	    		for(Symbol first : firstSet) {
+	    			if(first.getType() == SymbolType.NON_TERMINAL) {
+	    				if(tmp.addAll(firstSets.get(first))) {
+	    					change = true;
+	    				}
+	    			}
+	    		}
+	    		firstSets.put(symbol, tmp);
+	    	}
+        } while(change);
+
+    	// maknuti nezavrsne (i eps)
+    	for(Symbol symbol : nonTerminalSymbols) {
+    		firstSets.get(symbol).removeIf(
+   				new Predicate<Symbol>() {
+					@Override
+					public boolean test(Symbol sym) {
+						return sym.getType() == SymbolType.NON_TERMINAL || "$".equals(sym.toString());
+					}
+   				}
+    		);
+    	}
+    	
+//    	System.out.println("---\nProvjera ZAPOCINJE skupova:");
+//    	for(Symbol symbol : nonTerminalSymbols) {
+//    		System.out.print(symbol + " -> ");
+//    		for(Symbol sym : firstSets.get(symbol)) {
+//    			System.out.print(sym + " ");
+//    		}
+//    		System.out.println();
+//    	}
+//    	System.out.println("kraj\n---");
     }
 
 }
